8e2520221bf14005ddcff0d1f86dbcd507826429
周磊 <zhoulei@gridsum.com>
2021-04-15 10:43:42 +0800

    Redis适配SSL 测试

--- spring-boot-demo/src/test/java/com/dylan/web/demo/RedisStringTest.java
+++ spring-boot-demo/src/test/java/com/dylan/web/demo/RedisStringTest.java
@@ -5,27 +5,27 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RedisStringTest {
-
+    
     @Autowired
-    RedisTemplate<String, Object> redisTemplate;
-
+    StringRedisTemplate redisTemplate;
+    
     @Test
     public void testForSetValue() {
         String key = "test";
         String value = "test-value";
         this.redisTemplate.opsForValue().set(key, value);
     }
-
+    
     @Test
     public void testForGetValue() {
         Assert.assertEquals("test-value", this.redisTemplate.opsForValue().get("test"));
     }
-
-
+    
+    
 }
